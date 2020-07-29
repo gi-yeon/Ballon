a312b3ae92397281b6c59511d151fad026405cf1
gunwbro <gunwbro@gmail.com>
2020-07-29 21:39:27 +0900

    ㅎㅇ

---
+++ Assets/Load.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+    private Moving thePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<Moving>();
+        if (ES3.KeyExists("playerPosition") && ES3.KeyExists("btn"))
+        {
+            if (ES3.Load<BTNType>("btn") != BTNType.load)
+                return;
+
+            thePlayer.transform.position = ES3.Load<Vector3>("playerPosition");
+        }
+    }
+}
---
+++ Assets/Save.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour
+{
+    private Moving thePlayer;
+    private Vector3 playerPos;
+    void Start()
+    {
+        thePlayer = FindObjectOfType<Moving>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SaveData();
+            Debug.Log("저장되었습니다.");
+        }
+    }
+
+    void SaveData()
+    {
+        playerPos = thePlayer.transform.position;
+        ES3.Save<Vector3>("playerPosition", playerPos);
+    }
+}
